557678419ccf27a76d8e48affcbefded221d235b
Kjetil Østerås <kos@appeartv.com>
2014-03-27 20:28:32 +0100

    Initial blink01, which blinks too fast to see

---
+++ blink01/blink01.asm
@@ -0,0 +1,36 @@
+
+; use PIC 12F675
+list     p=12f675
+#include <p12f675.inc>
+
+; Configuration word 
+
+  __CONFIG   _CP_OFF & _CPD_OFF & _BODEN_OFF & _MCLRE_OFF & _WDT_OFF & _PWRTE_ON & _INTRC_OSC_NOCLKOUT 
+
+reset_vector code 0x0000  ; processor reset vector
+  goto main
+
+main
+
+; Calibrate internal oscillator according to the pic12f675 datasheet
+; the location of the factory adjustment value is 0x3ff and they have stored
+; an instruction 'retlw xx'
+
+  bsf STATUS,RP0    ; set file register bank to 1 
+  call 0x3ff        ; retrieve factory calibration value
+  movwf OSCCAL      ; update register with factory cal value 
+  bcf STATUS,RP0    ; set file register bank to 0
+
+  bsf STATUS,RP0 ; enter bank 1
+  movlw b'00001110'
+  movwf TRISIO ; configure I/O
+
+  bcf STATUS,RP0 ; enter bank 0
+flash
+  movlw b'00010000'
+  movwf GPIO  ; power on LED D0
+  movlw b'00000000'
+  movwf GPIO  ; power off LED D0
+  goto flash       ; loop forever
+
+  end
